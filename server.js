05fe0d75875aa8f855f4d077b77dde7c10c24135
Siddhartha PRadhan <sppradhan@wpi.edu>
2022-08-25 16:17:02 -0400

    Added main text and color schemes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http'),
       fs   = require('fs'),
       port = 3000
 
-const server = http.createServer( function( request,response ) {
+const server = http.createServer(function( request,response ) {
   switch( request.url ) {
     case '/':
       sendFile( response, 'index.html' )
@@ -10,8 +10,11 @@ const server = http.createServer( function( request,response ) {
     case '/index.html':
       sendFile( response, 'index.html' )
       break
+    case '/main.css':
+      sendFile(response, 'main.css')
+      break
     default:
-      response.end( '404 Error: File Not Found' )
+      response.end('404 Error: File Not Found' )
   }
 })
 
@@ -23,3 +26,36 @@ const sendFile = function( response, filename ) {
      response.end( content, 'utf-8' )
    })
 }
+
+console.log("Server Started")
+
+/*
+TODO:
+*Technical* 
+1. (max 5 points) Style your page using CSS. Each style rule you apply 
+    will get you 1 extra point for a maximum of 5 points. 
+    Be sure to describe your style rules in your README.
+2. (5 points) Add a simple JavaScript animation to the page.
+
+      -ADD Animation for sin wave and stuff after HEAD
+
+3. (max 5 points) Experiment with other HTML tags 
+    (links, images, tables etc.) Each extra tag you use will get 
+    you 1 extra point for a maximum of 5 points. 
+    Be sure to describe the links you use in your README.
+        -Added Table
+
+
+*Design*
+1. (10 points) Create a color palette using 
+    [color.adobe.com](https://color.adobe.com). 
+    Use all the colors in the palette in your webpage by 
+    implementing the appropriate CSS. Add a small screenshot 
+    of the color wheel for your color palette to your repo.
+
+    -Mint Works well, mint, white and blackbg
+
+2. (5 points) Use a font from [Goolge Fonts](https://fonts.google.com) 
+    in your website. 
+    - (DONE), loaded font and applied to head
+    */
