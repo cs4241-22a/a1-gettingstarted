4415fcd07e8142b6d58819a29666458fa7072a4b
Joe Dobbelaar <joedobbelaar@gmail.com>
2022-08-25 13:17:15 -0400

    Accordion hovering

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 window.onload = () => {
     const homeButton = document.getElementById("home-button");
-
+    const homeAccordion = document.getElementById("home-accordion");
     homeButton.addEventListener("mouseenter", () => {
         homeButton.classList.add("hover");
-    })
-    
+        homeAccordion.classList.add("hover");
+    })  
     homeButton.addEventListener("mouseleave", () => {
         homeButton.classList.remove("hover");
     })
