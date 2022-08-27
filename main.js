d888956f92bf8ba0307bd0c376c36c1c81ccf32a
Othniel9 <othnielbondah@gmail.com>
2022-08-27 13:23:48 -0400

    All done

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-let line = windows.getElementbyID("headline")
-windows.onclick(function("line")){
-  
-})
+let line
+function main() {
+    line = document.getElementById("headline");
+    line.onclick = function (event) {
+        window.alert("Welcome to my page for any questions please contact me on this email. `obondah@wpi.edu`")
+    }
+}
