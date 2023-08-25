bd93318ce27645c240c4d7d28aecd479f0c17f59
InDanity <101886886+InDanity@users.noreply.github.com>
2023-08-24 22:57:37 -0400

    Made terrible color palette.

--- server.js
+++ server.js
@@ -6,10 +6,14 @@ const server = http.createServer( function( request,response ) {
   switch( request.url ) {
     case '/':
       sendFile( response, 'index.html' )
+      sendFile( response, 'style.css' )
       break
     case '/index.html':
       sendFile( response, 'index.html' )
       break
+    case '/style.css':
+      sendFile( response, 'style.css' )
+      break;
     default:
       response.end( '404 Error: File Not Found' )
   }
